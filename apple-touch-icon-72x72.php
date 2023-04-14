8a355bfd27b2543dd1ed366dbef8a07ac4efa3c8
Biffman <127398687+Sviluppman@users.noreply.github.com>
2023-04-14 16:58:20 +0200

    first file
    first

---
+++ apple-touch-icon-72x72.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Prevent not found of Apple Touch Icon
+ * Description: Plugin to prevent the apple touch icon 404 error page
+ * Author: NDV Comunicazione - Sviluppo
+ * Version: 3.0
+ **/
